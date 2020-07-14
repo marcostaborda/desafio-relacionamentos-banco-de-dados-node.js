75599fd38731e824ba5ea8e9c4fc4f58de1c0ef3
Marcos Taborda <marcos.tabordamail@gmail.com>
2020-07-14 11:27:39 -0300

    Add Rest Client customers

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,11 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+    const customerService = container.resolve(CreateCustomerService);
+
+    const customer = await customerService.execute({ name, email });
+
+    return response.status(200).json(customer);
   }
 }
